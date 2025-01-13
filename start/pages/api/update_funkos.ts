62e458683db0ad90f1445206f880fcfc2ebcaae7
Jeff Blankenburg <github@jeffblankenburg.com>
2025-01-13 05:59:57 -0500

    removed hardcoding

--- start/pages/api/update_funkos.ts
+++ start/pages/api/update_funkos.ts
@@ -8,7 +8,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const newFunko: Funko = req.body;
     console.log(newFunko);
     try {
-      await (await clientPromise).db('funko-showcase').collection('funkos').updateOne({ "_id": new ObjectId(newFunko._id) }, { $set: { imageUrl: newFunko.imageUrl, source: newFunko.source, character: newFunko.character, yearReleased: newFunko.yearReleased, numberInLine: newFunko.numberInLine } });
+      var collection_name = process.env.COLLECTION_NAME;
+      await (await clientPromise).db(process.env.DATABASE_NAME).collection(collection_name).updateOne({ "_id": new ObjectId(newFunko._id) }, { $set: { imageUrl: newFunko.imageUrl, source: newFunko.source, character: newFunko.character, yearReleased: newFunko.yearReleased, numberInLine: newFunko.numberInLine } });
       res.status(201).send(newFunko);
     } catch (error) {
       res.status(400).send(error);
