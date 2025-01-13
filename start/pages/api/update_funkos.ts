0b7fc1d1c76f49fc21e8e98c8a4bf5ab8072fc80
Jeff Blankenburg <github@jeffblankenburg.com>
2025-01-13 06:05:55 -0500

    updates

--- start/pages/api/update_funkos.ts
+++ start/pages/api/update_funkos.ts
@@ -8,8 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const newFunko: Funko = req.body;
     console.log(newFunko);
     try {
-      var collection_name = process.env.COLLECTION_NAME;
-      await (await clientPromise).db(process.env.DATABASE_NAME).collection(collection_name).updateOne({ "_id": new ObjectId(newFunko._id) }, { $set: { imageUrl: newFunko.imageUrl, source: newFunko.source, character: newFunko.character, yearReleased: newFunko.yearReleased, numberInLine: newFunko.numberInLine } });
+      const collection_name = process.env.COLLECTION_NAME || 'defaultCollectionName';
+      const database_name = process.env.DATABASE_NAME || 'defaultDatabaseName';
+      await (await clientPromise).db(database_name).collection(collection_name).updateOne({ "_id": new ObjectId(newFunko._id) }, { $set: { imageUrl: newFunko.imageUrl, source: newFunko.source, character: newFunko.character, yearReleased: newFunko.yearReleased, numberInLine: newFunko.numberInLine } });
       res.status(201).send(newFunko);
     } catch (error) {
       res.status(400).send(error);
